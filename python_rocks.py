ebb8ddd71aba7259952e57d70177ae3fd46cb72b
dave <dwheelerau@gmail.com>
2016-11-25 14:00:35 +1300

    added a python file to mess with

---
+++ python_rocks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import this
+
+def byitself(number):
+    """ A simple function that does very little"""
+    return number * number
+
+
+myvar = 10
+
+for num in range(myvar):
+    print "The current number is %s" % num
+    if num == 9:
+        print "The last number is %s, python counts from 0!" % num
+    else:
+        print "In case you care %s^%s is %s" % (num, num, byitself(num))
